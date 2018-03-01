78bc678cd8f0f90351975a715eaca365a3199469
Didac Montero <didac.monteromendez@epfl.ch>
2018-03-01 19:40:46 +0100

    Added IdRef data type

--- modules/ld/src/main/scala/ch/epfl/bluebrain/nexus/admin/ld/Curie.scala
+++ modules/ld/src/main/scala/ch/epfl/bluebrain/nexus/admin/ld/Curie.scala
@@ -1,5 +1,6 @@
 package ch.epfl.bluebrain.nexus.admin.ld
 
+import cats.Show
 import ch.epfl.bluebrain.nexus.admin.ld.uri.{PrefixName, Reference}
 
 /**
@@ -9,3 +10,7 @@ import ch.epfl.bluebrain.nexus.admin.ld.uri.{PrefixName, Reference}
   * @param reference the reference component of the curie
   */
 final case class Curie(prefix: PrefixName, reference: Reference)
+
+object Curie {
+  implicit val curieShow: Show[Curie] = Show.show { case Curie(p, r) => s"$p:$r" }
+}
---
+++ modules/ld/src/main/scala/ch/epfl/bluebrain/nexus/admin/ld/IdRef.scala
@@ -0,0 +1,34 @@
+package ch.epfl.bluebrain.nexus.admin.ld
+
+import cats.Show
+import cats.syntax.show._
+import ch.epfl.bluebrain.nexus.admin.ld.uri.{Id, PrefixName, PrefixValue, Reference}
+import eu.timepit.refined.api.RefType._
+
+/**
+  * Data type which contains the information needed to build
+  * both a [[Curie]] and a [[Prefix]] as well as to build the expanded [[Id]]
+  *
+  * @param prefixName  the prefix name
+  * @param prefixValue the value to which the name expands,
+  *                    as well as the prefix component of the curie
+  * @param reference   the reference component of the curie
+  */
+final case class IdRef(prefixName: PrefixName, prefixValue: PrefixValue, reference: Reference) {
+
+  lazy val curie: Curie =
+    Curie(prefixName, reference)
+
+  lazy val prefix: Prefix =
+    Prefix(prefixName, prefixValue)
+
+  lazy val id: Id =
+    refinedRefType.unsafeWrap(s"${prefixValue.value}${reference.value}")
+}
+
+object IdRef {
+
+  final implicit val idRefShow: Show[IdRef] =
+    Show.show(id => id.curie.show)
+
+}
