1b02691ec056818e2b1f4a4d903e7c43b6ecc65c
Wojciech Wajerowicz <wojciech.wajerowicz@epfl.ch>
2018-04-16 17:50:01 +0200

    Fix query parameter names.

--- modules/service/src/test/scala/ch/epfl/bluebrain/nexus/admin/service/directives/QueryDirectivesSpec.scala
+++ modules/service/src/test/scala/ch/epfl/bluebrain/nexus/admin/service/directives/QueryDirectivesSpec.scala
@@ -64,7 +64,7 @@ class QueryDirectivesSpec
     }
 
     "extract provided page" in {
-      Get("/?from=1&size=30") ~> route {
+      Get("/?_from=1&_size=30") ~> route {
         Response(Pagination(1L, 30), QueryPayload(`@context` = defaultContext))
       } ~> check {
         responseAs[Json] shouldEqual success
@@ -72,7 +72,7 @@ class QueryDirectivesSpec
     }
 
     "extract 0 when size and from are negative" in {
-      Get("/?from=-1&size=-30") ~> route {
+      Get("/?_from=-1&_size=-30") ~> route {
         Response(Pagination(0L, 0), QueryPayload(`@context` = defaultContext))
       } ~> check {
         responseAs[Json] shouldEqual success
@@ -80,7 +80,7 @@ class QueryDirectivesSpec
     }
 
     "extract maximum page size when provided is greater" in {
-      Get("/?from=1&size=500") ~> route {
+      Get("/?_from=1&_size=500") ~> route {
         Response(Pagination(1L, pageConfig.maxSize), QueryPayload(`@context` = defaultContext))
       } ~> check {
         responseAs[Json] shouldEqual success
@@ -88,7 +88,7 @@ class QueryDirectivesSpec
     }
 
     "extract deprecated and q query params when provided" in {
-      Get("/?deprecated=false&q=something") ~> route {
+      Get("/?_deprecated=false&_q=something") ~> route {
         Response(pageConfig.pagination,
                  QueryPayload(`@context` = defaultContext, deprecated = Some(false), q = Some("something")))
       } ~> check {
@@ -97,17 +97,17 @@ class QueryDirectivesSpec
     }
 
     "extract the JSON-LD output format when provided" in {
-      Get("/?format=flattened") ~> route {
+      Get("/?_format=flattened") ~> route {
         Response(pageConfig.pagination, QueryPayload(`@context` = defaultContext, format = JsonLdFormat.Flattened))
       } ~> check {
         responseAs[Json] shouldEqual success
       }
-      Get("/?format=expanded") ~> route {
+      Get("/?_format=expanded") ~> route {
         Response(pageConfig.pagination, QueryPayload(`@context` = defaultContext, format = JsonLdFormat.Expanded))
       } ~> check {
         responseAs[Json] shouldEqual success
       }
-      Get("/?format=compacted") ~> route {
+      Get("/?_format=compacted") ~> route {
         Response(pageConfig.pagination, QueryPayload(`@context` = defaultContext, format = JsonLdFormat.Compacted))
       } ~> check {
         responseAs[Json] shouldEqual success
@@ -115,7 +115,7 @@ class QueryDirectivesSpec
     }
 
     "reject when an invalid JSON-LD output format is provided" in {
-      Get("/?format=foobar") ~> route() ~> check {
+      Get("/?_format=foobar") ~> route() ~> check {
         status shouldEqual StatusCodes.BadRequest
         responseAs[Error] shouldEqual Error("IllegalParam",
                                             Some("Could not find JsonLdFormat for 'foobar'"),
@@ -124,7 +124,7 @@ class QueryDirectivesSpec
     }
 
     "reject when invalid fields provided" in {
-      Get("/?fields=a,b,c") ~> route() ~> check {
+      Get("/?_fields=a,b,c") ~> route() ~> check {
         status shouldEqual StatusCodes.BadRequest
         responseAs[Error] shouldEqual Error("IllegalParam",
                                             Some("Could not expand field 'a': DownArray"),
@@ -133,7 +133,7 @@ class QueryDirectivesSpec
     }
 
     "reject when invalid JSON" in {
-      Get(s"/?context=${URLEncoder.encode("""{"a": b}]""", "UTF-8")}") ~> route() ~> check {
+      Get(s"/?_context=${URLEncoder.encode("""{"a": b}]""", "UTF-8")}") ~> route() ~> check {
         status shouldEqual StatusCodes.BadRequest
         responseAs[Error] shouldEqual Error("WrongOrInvalidJson",
                                             None,
@@ -142,7 +142,7 @@ class QueryDirectivesSpec
     }
 
     "extract fields, pagination, q and deprecated when provided" in {
-      Get("/?deprecated=true&q=something&from=1&size=30&fields=nxv:one,schema:two,rdf:three,,") ~> route {
+      Get("/?_deprecated=true&_q=something&_from=1&_size=30&_fields=nxv:one,schema:two,rdf:three,,") ~> route {
         Response(
           Pagination(1L, 30),
           QueryPayload(
@@ -160,7 +160,7 @@ class QueryDirectivesSpec
 
     "extract sort when provided" in {
       val rdfType = rdf.tpe.id.value.replace("#", "%23")
-      Get(s"/?sort=-nxv:_createdAtTime,${rdfType},,,") ~> route {
+      Get(s"/?_sort=-nxv:_createdAtTime,${rdfType},,,") ~> route {
         val expectedSort =
           SortList(List(Sort(OrderType.Desc, nxv.createdAtTime.id.value), Sort(OrderType.Asc, rdf.tpe.id.value)))
         Response(pageConfig.pagination, QueryPayload(`@context` = defaultContext, sort = expectedSort))
@@ -170,7 +170,7 @@ class QueryDirectivesSpec
     }
 
     "merge context when specified as JSON object" in {
-      Get(s"/?context=${URLEncoder.encode(JsonLD(addedContext).contextValue.noSpaces, "UTF-8")}") ~> route {
+      Get(s"/?_context=${URLEncoder.encode(JsonLD(addedContext).contextValue.noSpaces, "UTF-8")}") ~> route {
         Response(pageConfig.pagination, QueryPayload(`@context` = addedContext.contextValue deepMerge defaultContext))
       } ~> check {
         responseAs[Json] shouldEqual success
