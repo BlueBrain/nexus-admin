18e9e7d5dcb2e684abfad7007b33f71a1b9557f6
Didac <didac.montero.mendez@gmail.com>
2018-04-19 11:41:11 +0200

    Added url encoding on filter query param (#68)
    Fixes #67

--- docs/src/main/paradox/assets/api-reference/projects/list-projects.sh
+++ docs/src/main/paradox/assets/api-reference/projects/list-projects.sh
@@ -1,3 +1,3 @@
 curl -XGET 'https://nexus.example.com/v1/projects
       ?from=20&size=20&deprecated=true
-      &filter={"op":"lt","path":"nxv:rev","value":3}'
+      &filter=%7B%22op%22%3A%22lt%22%2C%22path%22%3A%22nxv%3Arev%22%2C%22value%22%3A3%7D'
