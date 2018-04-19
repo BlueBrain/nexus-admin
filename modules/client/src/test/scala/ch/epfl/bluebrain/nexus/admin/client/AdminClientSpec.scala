e8ed6c0b3882ae94053fb680fd35c95e12751520
Henry Genet <henry.genet@epfl.ch>
2018-04-19 16:47:09 +0200

    Try Refined workaround, check credentials in the mocked client

--- modules/client/src/test/scala/ch/epfl/bluebrain/nexus/admin/client/AdminClientSpec.scala
+++ modules/client/src/test/scala/ch/epfl/bluebrain/nexus/admin/client/AdminClientSpec.scala
@@ -3,6 +3,7 @@ package ch.epfl.bluebrain.nexus.admin.client
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.Uri.Path
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.{Authorization, OAuth2BearerToken}
 import akka.stream.{ActorMaterializer, Materializer}
 import akka.testkit.TestKit
 import ch.epfl.bluebrain.nexus.admin.client.config.AdminConfig
@@ -15,12 +16,12 @@ import ch.epfl.bluebrain.nexus.commons.iam.identity.Identity.UserRef
 import ch.epfl.bluebrain.nexus.commons.iam.identity.IdentityId
 import ch.epfl.bluebrain.nexus.commons.test.Resources.contentOf
 import ch.epfl.bluebrain.nexus.commons.types.HttpRejection.UnauthorizedAccess
-import eu.timepit.refined.auto._
+import eu.timepit.refined._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
-import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
 
 class AdminClientSpec
     extends TestKit(ActorSystem("IamClientSpec"))
@@ -45,39 +46,42 @@ class AdminClientSpec
   "An AdminClient" should {
 
     "return an existing project from upstream" in {
-      val name: ProjectReference = "projectname"
+      val name: ProjectReference = refineMV("projectname")
       val mockedResponse = Future.successful(
         HttpResponse(
           entity = HttpEntity(ContentTypes.`application/json`, contentOf("/project.json")),
           status = StatusCodes.OK
         ))
       implicit val cl: UntypedHttpClient[Future] =
-        mockedClient(base.copy(path = base.path + "projectname"), mockedResponse)
+        mockedClient(base.copy(path = base.path + "projectname"), Some(OAuth2BearerToken("validToken")), mockedResponse)
       val adminClient = AdminClient(config)
 
-      val project = adminClient.getProject(name, None).futureValue
+      val project = adminClient.getProject(name, Some(OAuth2BearerToken("validToken"))).futureValue
       project.deprecated shouldEqual false
       project.rev shouldEqual 3
       project.name shouldEqual name
       project.config shouldEqual Config(10)
       project.prefixMappings shouldEqual List(
-        LoosePrefixMapping("nxv-projectname", "https://nexus.example.com/vocabs/nexus/core/terms/v0.1.0/"),
-        LoosePrefixMapping("person-projectname", "https://shapes-registry.org/commons/person")
+        LoosePrefixMapping(refineMV("nxv-projectname"),
+                           refineMV("https://nexus.example.com/vocabs/nexus/core/terms/v0.1.0/")),
+        LoosePrefixMapping(refineMV("person-projectname"), refineMV("https://shapes-registry.org/commons/person"))
       )
     }
 
     "return ACLs from upstream for an existing project" in {
-      val name: ProjectReference = "projectname"
+      val name: ProjectReference = refineMV("projectname")
       val mockedResponse = Future.successful(
         HttpResponse(
           entity = HttpEntity(ContentTypes.`application/json`, contentOf("/project-acls.json")),
           status = StatusCodes.OK
         ))
       implicit val cl: UntypedHttpClient[Future] =
-        mockedClient(base.copy(path = base.path + "projectname/acls"), mockedResponse)
+        mockedClient(base.copy(path = base.path + "projectname/acls"),
+                     Some(OAuth2BearerToken("validToken")),
+                     mockedResponse)
       val adminClient = AdminClient(config)
 
-      val project = adminClient.getProjectAcls(name, None).futureValue
+      val project = adminClient.getProjectAcls(name, Some(OAuth2BearerToken("validToken"))).futureValue
       project.acl shouldEqual List(
         FullAccessControl(
           UserRef(
@@ -88,14 +92,14 @@ class AdminClientSpec
     }
 
     "forward unauthorized access errors" in {
-      val name: ProjectReference = "unauthorized"
+      val name: ProjectReference = refineMV("unauthorized")
       val mockedResponse = Future.successful(
         HttpResponse(
           entity = HttpEntity(ContentTypes.`application/json`, contentOf("/unauthorized.json")),
           status = StatusCodes.Unauthorized
         ))
       implicit val cl: UntypedHttpClient[Future] =
-        mockedClient(base.copy(path = base.path + "unauthorized"), mockedResponse)
+        mockedClient(base.copy(path = base.path + "unauthorized"), None, mockedResponse)
       val adminClient = AdminClient(config)
 
       val error = adminClient.getProject(name, None).failed.futureValue
@@ -103,14 +107,14 @@ class AdminClientSpec
     }
 
     "handle unexpected upstream errors" in {
-      val name: ProjectReference = "nonexistent"
+      val name: ProjectReference = refineMV("nonexistent")
       val mockedResponse = Future.successful(
         HttpResponse(
           entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, "Resource not found"),
           status = StatusCodes.NotFound
         ))
       implicit val cl: UntypedHttpClient[Future] =
-        mockedClient(base.copy(path = base.path + "nonexistent"), mockedResponse)
+        mockedClient(base.copy(path = base.path + "nonexistent"), None, mockedResponse)
       val adminClient = AdminClient(config)
 
       val error = adminClient.getProject(name, None).failed.futureValue
@@ -118,11 +122,23 @@ class AdminClientSpec
     }
   }
 
-  def mockedClient(expectedUri: Uri, response: Future[HttpResponse]): UntypedHttpClient[Future] =
+  def mockedClient(expectedUri: Uri,
+                   expectedAuth: Option[OAuth2BearerToken],
+                   response: Future[HttpResponse]): UntypedHttpClient[Future] =
     new UntypedHttpClient[Future] {
       override def apply(req: HttpRequest): Future[HttpResponse] =
-        if (req.uri == expectedUri) response
-        else fail(s"Wrong request uri: ${req.uri} expected: $expectedUri")
+        if (req.uri == expectedUri) {
+          (expectedAuth, req.header[Authorization]) match {
+            case (None, None) =>
+              response
+            case (Some(OAuth2BearerToken(t1)), Some(Authorization(OAuth2BearerToken(t2)))) if t1 == t2 =>
+              response
+            case _ =>
+              fail("Wrong request credentials")
+          }
+        } else {
+          fail(s"Wrong request uri: ${req.uri} expected: $expectedUri")
+        }
 
       override def discardBytes(entity: HttpEntity): Future[HttpMessage.DiscardedEntity] =
         Future.apply(entity.discardBytes())
