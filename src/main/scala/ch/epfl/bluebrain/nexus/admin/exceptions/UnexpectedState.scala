824d2454fb6c0421d9166d89305e225a9ee4eb0d
Wojciech Wajerowicz <wojciech.wajerowicz@epfl.ch>
2018-11-26 16:38:12 +0100

    Add IncorrectlyNamedExceptions supression.

--- src/main/scala/ch/epfl/bluebrain/nexus/admin/exceptions/UnexpectedState.scala
+++ src/main/scala/ch/epfl/bluebrain/nexus/admin/exceptions/UnexpectedState.scala
@@ -5,4 +5,5 @@ import ch.epfl.bluebrain.nexus.commons.types.Err
   *  Exception signalling that the resource is in an unexpected state
   * @param id ID of the resource
   */
+@SuppressWarnings(Array("IncorrectlyNamedExceptions"))
 class UnexpectedState(id: String) extends Err(s"Unexpected resource state for resource with ID $id")
