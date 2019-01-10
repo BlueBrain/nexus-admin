aaed669c5f47ce47ed5f0dd2a6748e1e24b31ebc
Wojciech Wajerowicz <wojciech.wajerowicz@epfl.ch>
2019-01-10 10:41:47 +0100

    Represent apiMappings consistently in the encoded events.

--- client/src/main/scala/ch/epfl/bluebrain/nexus/admin/client/types/events/decoders.scala
+++ client/src/main/scala/ch/epfl/bluebrain/nexus/admin/client/types/events/decoders.scala
@@ -1,8 +1,13 @@
 package ch.epfl.bluebrain.nexus.admin.client.types.events
 
+import java.time.Instant
+import java.util.UUID
+
+import ch.epfl.bluebrain.nexus.admin.client.types.events.ProjectEvent._
 import ch.epfl.bluebrain.nexus.iam.client.config.IamClientConfig
 import ch.epfl.bluebrain.nexus.iam.client.types.Identity
 import ch.epfl.bluebrain.nexus.iam.client.types.Identity.Subject
+import ch.epfl.bluebrain.nexus.rdf.Iri.AbsoluteIri
 import ch.epfl.bluebrain.nexus.rdf.instances._
 import ch.epfl.bluebrain.nexus.rdf.syntax.node.unsafe._
 import io.circe.Decoder
@@ -22,6 +27,12 @@ object decoders {
       case "organization" => "_organization"
       case other          => other
     })
+    .copy(transformConstructorNames = {
+      case "ProjectCreatedDto"    => "ProjectCreated"
+      case "ProjectUpdatedDto"    => "ProjectUpdated"
+      case "ProjectDeprecatedDto" => "ProjectDeprecated"
+      case other                  => other
+    })
 
   private implicit def subjectDecoder(implicit iamClientConfig: IamClientConfig): Decoder[Subject] =
     Decoder.decodeString.flatMap { id =>
@@ -32,16 +43,80 @@ object decoders {
 
     }
 
+  private implicit val mappingDecoder: Decoder[Mapping] = deriveDecoder[Mapping]
+
   /**
     * [[Decoder]] for [[ProjectEvent]]s.
     */
   implicit def projectEventDecoder(implicit iamClientConfig: IamClientConfig): Decoder[ProjectEvent] =
-    deriveDecoder[ProjectEvent]
+    deriveDecoder[ProjectEventDto].map(_.toEvent)
 
   /**
     * [[Decoder]] for [[OrganizationEvent]]s.
     */
   implicit def organizationEventDecoder(implicit iamClientConfig: IamClientConfig): Decoder[OrganizationEvent] =
     deriveDecoder[OrganizationEvent]
+
+  private sealed trait ProjectEventDto extends Product with Serializable {
+
+    def id: UUID
+
+    def rev: Long
+
+    def instant: Instant
+
+    def subject: Subject
+
+    def toEvent: ProjectEvent
+  }
+
+  private final case class Mapping(prefix: String, namespace: AbsoluteIri)
+
+  private final case class ProjectCreatedDto(id: UUID,
+                                             organization: UUID,
+                                             label: String,
+                                             description: Option[String],
+                                             apiMappings: List[Mapping],
+                                             base: AbsoluteIri,
+                                             instant: Instant,
+                                             subject: Subject)
+      extends ProjectEventDto {
+
+    val rev: Long = 1L
+    override def toEvent: ProjectEvent =
+      ProjectCreated(id,
+                     organization,
+                     label,
+                     description,
+                     apiMappings.map(m => (m.prefix, m.namespace)).toMap,
+                     base,
+                     instant,
+                     subject)
+  }
+
+  private final case class ProjectUpdatedDto(id: UUID,
+                                             label: String,
+                                             description: Option[String],
+                                             apiMappings: List[Mapping],
+                                             base: AbsoluteIri,
+                                             rev: Long,
+                                             instant: Instant,
+                                             subject: Subject)
+      extends ProjectEventDto {
+    override def toEvent: ProjectEvent =
+      ProjectUpdated(id,
+                     label,
+                     description,
+                     apiMappings.map(m => (m.prefix, m.namespace)).toMap,
+                     base,
+                     rev,
+                     instant,
+                     subject)
+  }
+
+  private final case class ProjectDeprecatedDto(id: UUID, rev: Long, instant: Instant, subject: Subject)
+      extends ProjectEventDto {
+    override def toEvent: ProjectEvent = ProjectDeprecated(id, rev, instant, subject)
+  }
 
 }
--- src/main/scala/ch/epfl/bluebrain/nexus/admin/kafka/encoders.scala
+++ src/main/scala/ch/epfl/bluebrain/nexus/admin/kafka/encoders.scala
@@ -3,8 +3,10 @@ package ch.epfl.bluebrain.nexus.admin.kafka
 import ch.epfl.bluebrain.nexus.admin.config.Contexts._
 import ch.epfl.bluebrain.nexus.admin.organizations.OrganizationEvent
 import ch.epfl.bluebrain.nexus.admin.projects.ProjectEvent
+import ch.epfl.bluebrain.nexus.admin.projects.ProjectEvent.{ProjectCreated, ProjectUpdated}
 import ch.epfl.bluebrain.nexus.iam.client.config.IamClientConfig
 import ch.epfl.bluebrain.nexus.iam.client.types.Identity.Subject
+import ch.epfl.bluebrain.nexus.rdf.Iri.AbsoluteIri
 import ch.epfl.bluebrain.nexus.rdf.instances._
 import ch.epfl.bluebrain.nexus.rdf.syntax.circe.context._
 import io.circe.generic.extras.Configuration
@@ -34,7 +36,15 @@ object encoders {
     */
   implicit def projectEventEncoder(implicit iamClientConfig: IamClientConfig): Encoder[ProjectEvent] = {
     val enc = deriveEncoder[ProjectEvent]
-    Encoder.instance(event => enc(event) deepMerge Json.obj("_rev" -> Json.fromLong(event.rev)).addContext(adminCtxUri))
+    Encoder.instance { event =>
+      val common = enc(event) deepMerge Json.obj("_rev" -> Json.fromLong(event.rev)).addContext(adminCtxUri)
+      event match {
+        case c: ProjectCreated => addApiMappings(common, c.apiMappings)
+        case u: ProjectUpdated => addApiMappings(common, u.apiMappings)
+        case _                 => common
+      }
+
+    }
   }
 
   /**
@@ -43,6 +53,16 @@ object encoders {
   implicit def organizationEventEncoder(implicit iamClientConfig: IamClientConfig): Encoder[OrganizationEvent] = {
     val enc = deriveEncoder[OrganizationEvent]
     Encoder.instance(event => enc(event) deepMerge Json.obj("_rev" -> Json.fromLong(event.rev)).addContext(adminCtxUri))
+  }
+
+  private def addApiMappings(json: Json, apiMappings: Map[String, AbsoluteIri]): Json = json.mapObject { obj =>
+    obj.add(
+      "apiMappings",
+      Json.arr(apiMappings.toList.map {
+        case (prefix, namespace) =>
+          Json.obj("prefix" -> Json.fromString(prefix), "namespace" -> Json.fromString(namespace.asString))
+      }: _*)
+    )
   }
 
 }
