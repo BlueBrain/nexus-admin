0ea8a7be0b59e3e1450327faba11610974e53958
Bogdan Roman <bogdan.roman@epfl.ch>
2019-01-18 15:07:42 +0100

    Refactored admin client

--- client/src/main/scala/ch/epfl/bluebrain/nexus/admin/client/AdminClient.scala
+++ client/src/main/scala/ch/epfl/bluebrain/nexus/admin/client/AdminClient.scala
@@ -1,42 +1,42 @@
 package ch.epfl.bluebrain.nexus.admin.client
+
+import akka.actor.ActorSystem
 import akka.http.scaladsl.client.RequestBuilding.Get
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.OAuth2BearerToken
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes}
-import akka.stream.ActorMaterializer
+import akka.http.scaladsl.unmarshalling.FromEntityUnmarshaller
+import akka.stream.{ActorMaterializer, Materializer}
 import cats.MonadError
-import cats.effect.LiftIO
-import cats.syntax.applicativeError._
-import cats.syntax.functor._
+import cats.effect.{IO, LiftIO}
+import cats.implicits._
+import ch.epfl.bluebrain.nexus.admin.client.AdminClientError.{UnknownError, UnmarshallingError}
 import ch.epfl.bluebrain.nexus.admin.client.config.AdminClientConfig
 import ch.epfl.bluebrain.nexus.admin.client.types.{Organization, Project}
+import ch.epfl.bluebrain.nexus.commons.http.HttpClient
 import ch.epfl.bluebrain.nexus.commons.http.HttpClient.UntypedHttpClient
-import ch.epfl.bluebrain.nexus.commons.http.{HttpClient, UnexpectedUnsuccessfulHttpResponse}
-import ch.epfl.bluebrain.nexus.commons.types.HttpRejection.UnauthorizedAccess
+import ch.epfl.bluebrain.nexus.commons.http.JsonLdCirceSupport._
+import ch.epfl.bluebrain.nexus.iam.client.IamClientError.{Forbidden, Unauthorized}
 import ch.epfl.bluebrain.nexus.iam.client.types.AuthToken
 import ch.epfl.bluebrain.nexus.rdf.Iri.Path
-import ch.epfl.bluebrain.nexus.rdf.syntax.akka._
 import ch.epfl.bluebrain.nexus.rdf.Iri.Path._
-import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+import ch.epfl.bluebrain.nexus.rdf.syntax.akka._
+import io.circe.{DecodingFailure, ParsingFailure}
 import journal.Logger
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
+import scala.reflect.ClassTag
 
 /**
   * Admin client.
   *
-  * @param config configuration for the client
-  * @param httpClient HTTP client
+  * @param cfg configuration for the client
   */
-class AdminClient[F[_]: LiftIO](config: AdminClientConfig, httpClient: UntypedHttpClient[F])(
-    implicit F: MonadError[F, Throwable],
-    ex: ExecutionContext,
-    mt: ActorMaterializer) {
-
-  private val log           = Logger[this.type]
-  private implicit val http = httpClient
-
-  private val projectsClient = HttpClient.withUnmarshaller[F, Project]
-  private val orgsClient     = HttpClient.withUnmarshaller[F, Organization]
+class AdminClient[F[_]](cfg: AdminClientConfig)(
+    implicit
+    F: MonadError[F, Throwable],
+    pc: HttpClient[F, Project],
+    oc: HttpClient[F, Organization],
+) {
 
   /**
     * Fetch [[Project]].
@@ -47,9 +47,11 @@ class AdminClient[F[_]: LiftIO](config: AdminClientConfig, httpClient: UntypedHt
     * @return [[Project]] instance if it exists, [[None]] otherwise, wrapped in [[F]]
     */
   def fetchProject(organization: String, label: String)(implicit credentials: Option[AuthToken]): F[Option[Project]] =
-    projectsClient(request("projects" / organization / label))
+    pc(request("projects" / organization / label))
       .map[Option[Project]](Some(_))
-      .recoverWith(recover(s"projects/$organization/$label"))
+      .recoverWith {
+        case UnknownError(StatusCodes.NotFound, _) => F.pure(None)
+      }
 
   /**
     * Fetch [[Organization]].
@@ -59,26 +61,14 @@ class AdminClient[F[_]: LiftIO](config: AdminClientConfig, httpClient: UntypedHt
     * @return [[Organization]] instance if it exists, [[None]] otherwise, wrapped in [[F]]
     */
   def fetchOrganization(label: String)(implicit credentials: Option[AuthToken]): F[Option[Organization]] =
-    orgsClient(request("orgs" / label))
+    oc(request("orgs" / label))
       .map[Option[Organization]](Some(_))
-      .recoverWith(recover(s"orgs/$label"))
-
-  private def recover[A](path: String): PartialFunction[Throwable, F[Option[A]]] = {
-    case UnexpectedUnsuccessfulHttpResponse(HttpResponse(StatusCodes.NotFound, _, _, _)) =>
-      F.pure(None)
-    case UnexpectedUnsuccessfulHttpResponse(HttpResponse(StatusCodes.Unauthorized, _, _, _)) =>
-      F.raiseError(UnauthorizedAccess)
-    case ur: UnexpectedUnsuccessfulHttpResponse =>
-      log.warn(
-        s"Received an unexpected response status code '${ur.response.status}' from Admin when attempting to fetch $path")
-      F.raiseError(ur)
-    case err =>
-      log.error(s"Received an unexpected exception from Admin when attempting to fetch $path", err)
-      F.raiseError(err)
-  }
+      .recoverWith {
+        case UnknownError(StatusCodes.NotFound, _) => F.pure(None)
+      }
 
   private def request(path: Path)(implicit credentials: Option[AuthToken]): HttpRequest =
-    addCredentials(Get((config.baseUri + path).toAkkaUri))
+    addCredentials(Get((cfg.baseUri + path).toAkkaUri))
 
   private def addCredentials(request: HttpRequest)(implicit credentials: Option[AuthToken]): HttpRequest =
     credentials.map(token => request.addCredentials(OAuth2BearerToken(token.value))).getOrElse(request)
@@ -86,16 +76,70 @@ class AdminClient[F[_]: LiftIO](config: AdminClientConfig, httpClient: UntypedHt
 
 object AdminClient {
 
+  private def httpClient[F[_], A: ClassTag](
+      implicit L: LiftIO[F],
+      F: MonadError[F, Throwable],
+      ec: ExecutionContext,
+      mt: Materializer,
+      cl: UntypedHttpClient[F],
+      um: FromEntityUnmarshaller[A]
+  ): HttpClient[F, A] = new HttpClient[F, A] {
+    private val logger = Logger(s"AdminHttpClient[${implicitly[ClassTag[A]]}]")
+
+    override def apply(req: HttpRequest): F[A] =
+      cl.apply(req).flatMap { resp =>
+        resp.status match {
+          case StatusCodes.Unauthorized =>
+            cl.toString(resp.entity).flatMap { entityAsString =>
+              F.raiseError[A](Unauthorized(entityAsString))
+            }
+          case StatusCodes.Forbidden =>
+            logger.error(s"Received Forbidden when accessing '${req.method.name()} ${req.uri.toString()}'.")
+            cl.toString(resp.entity).flatMap { entityAsString =>
+              F.raiseError[A](Forbidden(entityAsString))
+            }
+          case other if other.isSuccess() =>
+            val value = L.liftIO(IO.fromFuture(IO(um(resp.entity))))
+            value.recoverWith {
+              case pf: ParsingFailure =>
+                logger.error(
+                  s"Failed to parse a successful response of '${req.method.name()} ${req.getUri().toString}'.")
+                F.raiseError[A](UnmarshallingError(pf.getMessage()))
+              case df: DecodingFailure =>
+                logger.error(
+                  s"Failed to decode a successful response of '${req.method.name()} ${req.getUri().toString}'.")
+                F.raiseError(UnmarshallingError(df.getMessage()))
+            }
+          case other =>
+            cl.toString(resp.entity).flatMap { entityAsString =>
+              logger.error(
+                s"Received '${other.value}' when accessing '${req.method.name()} ${req.uri.toString()}', response entity as string: '$entityAsString.'")
+              F.raiseError[A](UnknownError(other, entityAsString))
+            }
+        }
+      }
+
+    override def discardBytes(entity: HttpEntity): F[HttpMessage.DiscardedEntity] =
+      cl.discardBytes(entity)
+
+    override def toString(entity: HttpEntity): F[String] =
+      cl.toString(entity)
+  }
+
   /**
     * Construct [[AdminClient]].
     *
-    * @param config     configuration for the client
-    * @param httpClient HTTP client
+    * @param cfg configuration for the client
     * @return new instance of [[AdminClient]]
     */
-  def apply[F[_]: LiftIO](config: AdminClientConfig, httpClient: UntypedHttpClient[F])(
-      implicit F: MonadError[F, Throwable],
-      ex: ExecutionContext,
-      mt: ActorMaterializer): AdminClient[F] =
-    new AdminClient(config, httpClient)
+  def apply[F[_]: LiftIO](cfg: AdminClientConfig)(implicit F: MonadError[F, Throwable],
+                                                  as: ActorSystem): AdminClient[F] = {
+    implicit val mt: ActorMaterializer        = ActorMaterializer()
+    implicit val ec: ExecutionContextExecutor = as.dispatcher
+    implicit val ucl: UntypedHttpClient[F]    = HttpClient.untyped[F]
+
+    implicit val pc: HttpClient[F, Project]      = httpClient[F, Project]
+    implicit val oc: HttpClient[F, Organization] = httpClient[F, Organization]
+    new AdminClient(cfg)
+  }
 }
--- client/src/test/scala/ch/epfl/bluebrain/nexus/admin/client/AdminClientSpec.scala
+++ client/src/test/scala/ch/epfl/bluebrain/nexus/admin/client/AdminClientSpec.scala
@@ -3,7 +3,6 @@ package ch.epfl.bluebrain.nexus.admin.client
 import java.time.Instant
 import java.util.UUID
 
-import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.client.RequestBuilding.Get
 import akka.http.scaladsl.model._
@@ -11,21 +10,24 @@ import akka.http.scaladsl.model.headers.OAuth2BearerToken
 import akka.stream.ActorMaterializer
 import akka.testkit.TestKit
 import cats.effect.IO
+import ch.epfl.bluebrain.nexus.admin.client.AdminClientError._
 import ch.epfl.bluebrain.nexus.admin.client.config.AdminClientConfig
 import ch.epfl.bluebrain.nexus.admin.client.types.{Organization, Project}
-import ch.epfl.bluebrain.nexus.commons.http.HttpClient.UntypedHttpClient
-import ch.epfl.bluebrain.nexus.commons.test.{Randomness, Resources}
+import ch.epfl.bluebrain.nexus.commons.http.HttpClient
 import ch.epfl.bluebrain.nexus.commons.test.io.IOOptionValues
-import ch.epfl.bluebrain.nexus.commons.types.HttpRejection.UnauthorizedAccess
+import ch.epfl.bluebrain.nexus.commons.test.{Randomness, Resources}
+import ch.epfl.bluebrain.nexus.iam.client.IamClientError
 import ch.epfl.bluebrain.nexus.iam.client.types.AuthToken
 import ch.epfl.bluebrain.nexus.rdf.syntax.node.unsafe._
 import org.mockito.Mockito
 import org.mockito.integrations.scalatest.IdiomaticMockitoFixture
+import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{BeforeAndAfter, Matchers, WordSpecLike}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 
+//noinspection NameBooleanParameters
 class AdminClientSpec
     extends TestKit(ActorSystem("AdminClientSpec"))
     with WordSpecLike
@@ -35,35 +37,43 @@ class AdminClientSpec
     with IdiomaticMockitoFixture
     with Randomness
     with IOOptionValues
+    with EitherValues
+    with Inspectors
     with Resources {
+
+  override implicit val patienceConfig: PatienceConfig = PatienceConfig(5 seconds, 15 milliseconds)
 
   implicit val ec: ExecutionContext  = system.dispatcher
   implicit val mt: ActorMaterializer = ActorMaterializer()
 
-  private val config     = AdminClientConfig(url"http://admin.nexus.example.com/v1".value)
-  private val httpClient = mock[UntypedHttpClient[IO]]
-  private val client     = new AdminClient[IO](config, httpClient)
+  private val config = AdminClientConfig(url"http://admin.nexus.example.com/v1".value)
+  private val token  = OAuth2BearerToken("token")
 
-  implicit val tokenOpt = Option(AuthToken("token"))
-  val token             = OAuth2BearerToken("token")
+  private implicit val pc: HttpClient[IO, Project]      = mock[HttpClient[IO, Project]]
+  private implicit val oc: HttpClient[IO, Organization] = mock[HttpClient[IO, Organization]]
+  private implicit val tokenOpt: Option[AuthToken]      = Option(AuthToken("token"))
 
-  val discardedEntity = new HttpMessage.DiscardedEntity(Future.successful(Done))
+  private val client = new AdminClient[IO](config)
+
+  private val org  = jsonContentOf("/organization.json").as[Organization].right.value
+  private val proj = jsonContentOf("/project.json").as[Project].right.value
+
+  private def orgRequest(label: String) =
+    Get(s"http://admin.nexus.example.com/v1/orgs/$label").addCredentials(token)
+
+  private def projRequest(org: String, label: String) =
+    Get(s"http://admin.nexus.example.com/v1/projects/$org/$label").addCredentials(token)
 
   before {
-    Mockito.reset(httpClient)
-    httpClient.discardBytes(any[HttpEntity]) shouldReturn IO.pure(discardedEntity)
+    Mockito.reset(pc, oc)
   }
 
-  "Admin client" should {
+  "The AdminClient" should {
 
     "fetch organization" in {
-
-      httpClient(Get(s"http://admin.nexus.example.com/v1/orgs/testorg").addCredentials(token)) shouldReturn IO
-        .pure(
-          HttpResponse(StatusCodes.OK,
-                       entity = HttpEntity(ContentTypes.`application/json`, contentOf("/organization.json"))))
-
-      client.fetchOrganization("testorg").some shouldEqual Organization(
+      val orgLabel = genString()
+      oc(orgRequest(orgLabel)) shouldReturn IO.pure(org)
+      client.fetchOrganization(orgLabel).some shouldEqual Organization(
         url"http://admin.nexus.example.com/v1/orgs/testorg".value,
         "testorg",
         "Test organization",
@@ -79,119 +89,70 @@ class AdminClientSpec
 
     "return None if the organization doesn't exist" in {
       val orgLabel = genString()
-
-      httpClient(Get(s"http://admin.nexus.example.com/v1/orgs/$orgLabel").addCredentials(token)) shouldReturn IO
-        .pure(HttpResponse(StatusCodes.NotFound, entity = HttpEntity(ContentTypes.`application/json`, "[]")))
-
-      client.fetchOrganization(orgLabel).unsafeRunSync() shouldEqual None
-    }
-    "fail with UnauthorizedAccess when fetching project" in {
-      val orgLabel = genString()
-
-      httpClient(Get(s"http://admin.nexus.example.com/v1/orgs/$orgLabel").addCredentials(token)) shouldReturn IO
-        .pure(HttpResponse(StatusCodes.Unauthorized, entity = HttpEntity(ContentTypes.`application/json`, "[]")))
-
-      client.fetchOrganization(orgLabel).unsafeToFuture().failed.futureValue shouldEqual UnauthorizedAccess
+      oc(orgRequest(orgLabel)) shouldReturn IO.raiseError(UnknownError(StatusCodes.NotFound, ""))
+      client.fetchOrganization(orgLabel).ioValue shouldEqual None
     }
 
-    "fail with other error when fetching organization" in {
-      val orgLabel  = genString()
-      val exception = new RuntimeException()
-
-      httpClient(Get(s"http://admin.nexus.example.com/v1/orgs/$orgLabel").addCredentials(token)) shouldReturn IO {
-        throw exception
+    "propagate the underlying exception when fetching organizations" in {
+      val exs: List[Exception] = List(
+        IamClientError.Unauthorized(""),
+        IamClientError.Forbidden(""),
+        AdminClientError.UnmarshallingError[Organization](""),
+        AdminClientError.UnknownError(StatusCodes.InternalServerError, "")
+      )
+      forAll(exs) { ex =>
+        val orgLabel = genString()
+        oc(orgRequest(orgLabel)) shouldReturn IO.raiseError(ex)
+        client.fetchOrganization(orgLabel).failed[Exception] shouldEqual ex
       }
-
-      client.fetchOrganization(orgLabel).unsafeToFuture().failed.futureValue shouldEqual exception
     }
 
-    "fetch project with optional fields" in {
-      httpClient(Get(s"http://admin.nexus.example.com/v1/projects/testorg/testproject").addCredentials(token)) shouldReturn IO
-        .pure(
-          HttpResponse(StatusCodes.OK,
-                       entity = HttpEntity(ContentTypes.`application/json`, contentOf("/project.json"))))
-
-      client.fetchProject("testorg", "testproject").some shouldEqual
-        Project(
-          url"http://admin.nexus.example.com/v1/projects/testorg/testproject".value,
-          "testproject",
-          "testorg",
-          Some("Test project"),
-          url"https://nexus.example.com/base".value,
-          url"https://nexus.example.com/voc".value,
-          Map(
-            "nxv" -> url"https://bluebrain.github.io/nexus/vocabulary/".value,
-            "rdf" -> url"http://www.w3.org/1999/02/22-rdf-syntax-ns#".value
-          ),
-          UUID.fromString("db7cee63-3f93-4d4a-9cc2-ebdace7f3b4f"),
-          UUID.fromString("504b6940-1b14-43a7-80e3-d08c52c3fc87"),
-          1L,
-          deprecated = false,
-          Instant.parse("2018-12-19T11:31:30.00Z"),
-          url"http://iam.nexus.example.com/v1/realms/example-realm/users/example-user".value,
-          Instant.parse("2018-12-20T11:31:30.00Z"),
-          url"http://iam.nexus.example.com/v1/realms/example-realm/users/example-user2".value
-        )
-    }
-
-    "fetch project without optional fields" in {
-      httpClient(Get(s"http://admin.nexus.example.com/v1/projects/testorg/testproject").addCredentials(token)) shouldReturn IO
-        .pure(
-          HttpResponse(StatusCodes.OK,
-                       entity = HttpEntity(ContentTypes.`application/json`, contentOf("/minimal-project.json"))))
-
-      client.fetchProject("testorg", "testproject").some shouldEqual
-        Project(
-          url"http://admin.nexus.example.com/v1/projects/testorg/testproject".value,
-          "testproject",
-          "testorg",
-          None,
-          url"https://nexus.example.com/base".value,
-          url"https://nexus.example.com/voc".value,
-          Map(
-            "nxv" -> url"https://bluebrain.github.io/nexus/vocabulary/".value,
-            "rdf" -> url"http://www.w3.org/1999/02/22-rdf-syntax-ns#".value
-          ),
-          UUID.fromString("db7cee63-3f93-4d4a-9cc2-ebdace7f3b4f"),
-          UUID.fromString("504b6940-1b14-43a7-80e3-d08c52c3fc87"),
-          1L,
-          deprecated = false,
-          Instant.parse("2018-12-19T11:31:30.00Z"),
-          url"http://iam.nexus.example.com/v1/realms/example-realm/users/example-user".value,
-          Instant.parse("2018-12-20T11:31:30.00Z"),
-          url"http://iam.nexus.example.com/v1/realms/example-realm/users/example-user2".value
-        )
+    "fetch a project" in {
+      val orgLabel     = genString()
+      val projectLabel = genString()
+      pc(projRequest(orgLabel, projectLabel)) shouldReturn IO.pure(proj)
+      client.fetchProject(orgLabel, projectLabel).some shouldEqual Project(
+        url"http://admin.nexus.example.com/v1/projects/testorg/testproject".value,
+        "testproject",
+        "testorg",
+        Some("Test project"),
+        url"https://nexus.example.com/base".value,
+        url"https://nexus.example.com/voc".value,
+        Map(
+          "nxv" -> url"https://bluebrain.github.io/nexus/vocabulary/".value,
+          "rdf" -> url"http://www.w3.org/1999/02/22-rdf-syntax-ns#".value
+        ),
+        UUID.fromString("db7cee63-3f93-4d4a-9cc2-ebdace7f3b4f"),
+        UUID.fromString("504b6940-1b14-43a7-80e3-d08c52c3fc87"),
+        1L,
+        deprecated = false,
+        Instant.parse("2018-12-19T11:31:30.00Z"),
+        url"http://iam.nexus.example.com/v1/realms/example-realm/users/example-user".value,
+        Instant.parse("2018-12-20T11:31:30.00Z"),
+        url"http://iam.nexus.example.com/v1/realms/example-realm/users/example-user2".value
+      )
     }
 
     "return None if the project doesn't exist" in {
       val orgLabel     = genString()
       val projectLabel = genString()
-
-      httpClient(Get(s"http://admin.nexus.example.com/v1/projects/$orgLabel/$projectLabel").addCredentials(token)) shouldReturn IO
-        .pure(HttpResponse(StatusCodes.NotFound, entity = HttpEntity(ContentTypes.`application/json`, "[]")))
-
+      pc(projRequest(orgLabel, projectLabel)) shouldReturn IO.raiseError(UnknownError(StatusCodes.NotFound, ""))
       client.fetchProject(orgLabel, projectLabel).unsafeRunSync() shouldEqual None
     }
-    "fail with UnauthorizedAccess when fetching organization" in {
-      val orgLabel     = genString()
-      val projectLabel = genString()
 
-      httpClient(Get(s"http://admin.nexus.example.com/v1/projects/$orgLabel/$projectLabel").addCredentials(token)) shouldReturn IO
-        .pure(HttpResponse(StatusCodes.Unauthorized, entity = HttpEntity(ContentTypes.`application/json`, "[]")))
-
-      client.fetchProject(orgLabel, projectLabel).unsafeToFuture().failed.futureValue shouldEqual UnauthorizedAccess
-    }
-
-    "fail with other error when fetching project" in {
-      val orgLabel     = genString()
-      val projectLabel = genString()
-      val exception    = new RuntimeException()
-
-      httpClient(Get(s"http://admin.nexus.example.com/v1/projects/$orgLabel/$projectLabel").addCredentials(token)) shouldReturn IO {
-        throw exception
+    "propagate the underlying exception when fetching projects" in {
+      val exs: List[Exception] = List(
+        IamClientError.Unauthorized(""),
+        IamClientError.Forbidden(""),
+        AdminClientError.UnmarshallingError[Organization](""),
+        AdminClientError.UnknownError(StatusCodes.InternalServerError, "")
+      )
+      forAll(exs) { ex =>
+        val orgLabel     = genString()
+        val projectLabel = genString()
+        pc(projRequest(orgLabel, projectLabel)) shouldReturn IO.raiseError(ex)
+        client.fetchProject(orgLabel, projectLabel).failed[Exception] shouldEqual ex
       }
-
-      client.fetchProject(orgLabel, projectLabel).unsafeToFuture().failed.futureValue shouldEqual exception
     }
   }
 
